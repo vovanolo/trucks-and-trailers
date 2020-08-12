93305f82c1a7d892e4edbd4b765bca61853222bd
vovanolo <vovchik.student221@gmail.com>
2020-08-12 16:48:24 +0300

    feat: the logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Menu } from 'antd';
+import { Menu, Button } from 'antd';
 import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeUser } from '../actions/users.actions';
+import app from '../feathers-client';
+
 
 export default function Navbar() {
   const location = useLocation();
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.usersReducer);
   const [currentLocation, setCurrentLocation] = useState(() => location);
 
@@ -14,18 +18,29 @@ export default function Navbar() {
     setCurrentLocation(pageName);
   }, [location]);
 
+  function logout() {
+    app.logout();
+    dispatch(removeUser());
+  }
+
   return (
     <Menu mode="horizontal" selectedKeys={[currentLocation]}>
       <Menu.Item key="home">
         <Link to="/">Home</Link>
       </Menu.Item>
-      {user === null ? (
+      {user === null && (
         <Menu.Item key="login">
           <Link to="/login">Login</Link>
         </Menu.Item>
-      ) : (
+      )}
+      {user !== null && (
         <Menu.Item>
           {user.user.email}
+        </Menu.Item>
+      )}
+      {user !== null && (
+        <Menu.Item>
+          <Button onClick={logout}>Logout</Button>
         </Menu.Item>
       )}
     </Menu>

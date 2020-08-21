d3396b910912cb3dd5e7e127ed460aa3af1f4531
olehhapuk <oleggapuk20@gmail.com>
2020-08-21 21:06:40 +0300

    feat: added signup form to admin page

--- src/pages/Admin/SignUp.jsx
+++ src/pages/Admin/SignUp.jsx
@@ -1,38 +1,80 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, Row, Col, PageHeader } from 'antd';
-import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import app from '../../express-client';
 
 export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const history = useHistory();
+
+  function signUpUser() {
+    app.create('users', {
+      username,
+      password
+    }, true)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.dir(error));
+  }
+  
   return (
     <>
-      <PageHeader title="Login" />
+      <PageHeader
+        title="Add new User"
+        onBack={() => history.goBack()}
+      />
       <Row justify="center">
         <Col span={6} md={6} sm={16} xs={20}>
           <Form
             name="loginForm"
             layout="vertical"
             initialValues={{ remember: true }}
+            onFinish={signUpUser}
           >
             <Form.Item
               label="Username"
               name="username"
+              hasFeedback
               rules={[{ required: true, message: 'Please type in your Username' }]}
             >
-              <Input type="text" placeholder="Username" />
+              <Input type="text" placeholder="Username" onChange={(e) => setUsername(e.currentTarget.value)} />
             </Form.Item>
 
             <Form.Item
               label="Password"
               name="password"
+              hasFeedback
               rules={[{ required: true, message: 'Please type in your Password' }]}
             >
-              <Input.Password placeholder="Password" />
+              <Input.Password placeholder="Password" onChange={(e) => setPassword(e.currentTarget.value)} />
             </Form.Item>
 
-            <Button type="primary" htmlType="submit">Login</Button>
+            <Form.Item
+              label="Repeat Password"
+              name="repeatPassword"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please repeat your Password'
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject('Passwords do not match!');
+                  },
+                }),
+              ]}
+            >
+              <Input.Password placeholder="Repeat Password" />
+            </Form.Item>
+
+            <Button type="primary" htmlType="submit">Sign Up</Button>
           </Form>
         </Col>
       </Row>

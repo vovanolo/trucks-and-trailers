6bcad1d7ada93dd370e43854848d6187a08b4958
olehhapuk <oleggapuk20@gmail.com>
2020-10-19 13:10:23 +0300

    feat: nothing changed

--- src/pages/Trailers.jsx
+++ src/pages/Trailers.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Button, PageHeader, Modal, Form, Spin, Result } from 'antd';
+import {
+  Table,
+  Space,
+  Button,
+  PageHeader,
+  Popconfirm,
+  Form,
+  Spin,
+  Result,
+} from 'antd';
 import { Link, useRouteMatch } from 'react-router-dom';
 
 import app from '../express-client';
 import { getFormattedError } from '../helpers';
-import TrailerForm from '../components/TrailerForm';
+
+import EditableCell from '../components/EditableCell';
 
 let mounted = true;
 
 export default function Trailers() {
+  const [form] = Form.useForm();
   const [trailers, setTrailers] = useState([]);
-  const [modalShow, setModalShow] = useState(false);
+  const [editingKey, setEditingKey] = useState('');
 
   const [error, setError] = useState(null);
   const [isRequestPending, setIsRequestPending] = useState(false);
@@ -22,7 +33,7 @@ export default function Trailers() {
     setIsRequestPending(true);
 
     app
-      .find('trailers', true)
+      .find('trailer', true)
       .then((res) => {
         if (mounted) {
           setIsRequestPending(false);
@@ -47,6 +58,62 @@ export default function Trailers() {
       mounted = false;
     };
   }, []);
+
+  function isEditingTrailer(record) {
+    return record.key === editingKey;
+  }
+
+  function startEditingTrailer(record) {
+    form.setFieldsValue({
+      name: '',
+      comment: '',
+      ownedByCompany: false,
+      rate: 0,
+      ...record,
+    });
+    setEditingKey(record.key);
+  }
+
+  async function updateTrailer(key) {
+    try {
+      const row = await form.validateFields();
+      const rowWithId = {
+        ...row,
+        id: key,
+      };
+
+      setIsRequestPending(true);
+
+      await app.update('trailers', rowWithId, true);
+
+      if (mounted) {
+        setIsRequestPending(false);
+
+        const newData = [...trailers];
+        const index = newData.findIndex((item) => key === item.key);
+
+        if (index > -1) {
+          const item = newData[index];
+          newData.splice(index, 1, { ...item, ...row });
+          setTrailers(newData);
+          setEditingKey('');
+        } else {
+          newData.push(row);
+          setTrailers(newData);
+          setEditingKey('');
+        }
+      }
+    } catch (error) {
+      if (mounted) {
+        setIsRequestPending(false);
+        setError(getFormattedError(error));
+      }
+    }
+  }
+
+  function cancelEditingTrailer() {
+    setEditingKey('');
+  }
 
   function removeTrailer(id) {
     setIsRequestPending(true);
@@ -88,15 +155,6 @@ export default function Trailers() {
       editable: true,
     },
     {
-      title: 'Location',
-      dataIndex: 'location',
-      key: 'location',
-      render: (data) => (data === null ? 'Unset' : data),
-      sorter: (a, b) => ('' + a.location).localeCompare(b.location),
-      responsive: ['lg'],
-      editable: true,
-    },
-    {
       title: 'Comment',
       dataIndex: 'comment',
       key: 'comment',
@@ -104,29 +162,69 @@ export default function Trailers() {
       editable: true,
     },
     {
-      title: 'Company',
-      dataIndex: 'Company',
-      key: 'Company',
-      render: (data) => (!data ? 'Unset' : data.name),
-      editable: false,
+      title: 'Rate',
+      dataIndex: 'rate',
+      key: 'rate',
+      sorter: (a, b) => a.rate - b.rate,
+      render: (data) => (data === null ? 'Unset' : data),
+      editable: true,
     },
     {
       title: 'Action',
       key: 'action',
-      render: () => (
-        <>
-          <Button
-            style={{ marginRight: 5 }}
-            type="primary"
-            onClick={() => setModalShow(true)}
-          >
-            Edit
-          </Button>
-          <Button danger>Delete</Button>
-        </>
-      ),
+      render: (_, record) => {
+        const editable = isEditingTrailer(record);
+        return editable ? (
+          <span>
+            <Button
+              onClick={() => updateTrailer(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Save
+            </Button>
+            <Popconfirm
+              title="Sure to cancel?"
+              onConfirm={cancelEditingTrailer}
+            >
+              <Button type="link">Cancel</Button>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Space size="middle">
+            <Button
+              type="default"
+              disabled={editingKey !== ''}
+              onClick={() => startEditingTrailer(record)}
+            >
+              Edit
+            </Button>
+            <Button type="link" onClick={() => removeTrailer(record.id)}>
+              Delete
+            </Button>
+          </Space>
+        );
+      },
     },
   ];
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'rate' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditingTrailer(record),
+      }),
+    };
+  });
 
   return (
     <Spin spinning={isRequestPending}>
@@ -139,21 +237,24 @@ export default function Trailers() {
         ]}
       />
 
-      <Modal visible={modalShow}>
-        <TrailerForm />
-      </Modal>
-
       {trailers !== [] && !error ? (
-        <Table
-          pagination={{
-            defaultCurrent: 1,
-            defaultPageSize: 9,
-            total: trailers.count,
-          }}
-          scroll={{ x: '100vw' }}
-          columns={columns}
-          dataSource={trailers}
-        />
+        <Form form={form} component={false}>
+          <Table
+            pagination={{
+              defaultCurrent: 1,
+              defaultPageSize: 9,
+              total: trailers.count,
+            }}
+            components={{
+              body: {
+                cell: EditableCell,
+              },
+            }}
+            scroll={{ x: '100vw' }}
+            columns={mergedColumns}
+            dataSource={trailers}
+          />
+        </Form>
       ) : (
         error && (
           <Result
